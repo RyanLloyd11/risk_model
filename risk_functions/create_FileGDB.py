faaf4e41fa9915da0a78932cdb68b97a3327414f
RyanLloyd11 <RyanLloyd@geollect.com>
2018-10-24 11:54:58 +0100

    Create create_FileGDB.py module

---
+++ risk_functions/create_FileGDB.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import os
+
+def creategdb(FileGDB_path,FileGDB_name):
+
+	if not os.path.exists(FileGDB_path + FileGDB_name): 
+    		arcpy.CreateFileGDB_management(FileGDB_path, FileGDB_name)
+	else:
+    		print("FileGDB already exists")
